ca9fff5f95bf5cbc9c764eea6bc438994220f681
Tom Shaw <tomshaw21@gmail.com>
2024-02-29 10:20:41 -0600

    Added button event dispatch.

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -16,10 +16,6 @@ $save = function () {
     $this->validate($rules);
 };
 
-$handleModalButton = function () {
-    $this->dispatch('mediable:open', id: 'description');
-};
-
 layout('layouts.app');
 ?>
 
@@ -76,7 +72,7 @@ layout('layouts.app');
                                 <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                                     Save
                                 </button>
-                                <button type="button" class="inline-flex items-center px-4 py-2 bg-pink-600 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-pink-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" wire:click.prevent="handleModalButton()">
+                                <button type="button" class="inline-flex items-center px-4 py-2 bg-pink-600 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-pink-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" wire:click="$dispatch('mediable:open', { id: 'description' })">
                                     Insert Media
                                 </button>
                             </div>
